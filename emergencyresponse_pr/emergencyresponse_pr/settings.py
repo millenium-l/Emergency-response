1811466089ada64c217e85df8b6aa4c1715d6d3e
millenium-1 <louiskaroki2010@gmail.com>
2026-03-16 11:34:14 +0300

    allowed hosts for all

--- emergencyresponse_pr/emergencyresponse_pr/settings.py
+++ emergencyresponse_pr/emergencyresponse_pr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+6wqzq-o4bp_wzc!0y092ln0@y&h!$f5iaj^fo3-f4rp_xb-@8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.100.4', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
