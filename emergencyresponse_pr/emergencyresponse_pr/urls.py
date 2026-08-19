5a39520eaf91cb0ec16cfcf32b3c169e1a7c1a56
millenium-1 <louiskaroki2010@gmail.com>
2026-08-19 13:02:00 +0300

    changed the first url redirect from home to login

--- emergencyresponse_pr/emergencyresponse_pr/urls.py
+++ emergencyresponse_pr/emergencyresponse_pr/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('login')),  # Redirect root URL to login page
+
     path('', include('emergencyresponse_app.urls')),
     path('accounts/', include('accounts.urls')),
 ]
