7f33986d673c028b45a7aff27b63f2ea7a826bb1
millenium-1 <louiskaroki2010@gmail.com>
2026-08-29 21:56:32 +0300

    added a env file to store secrete keys

--- emergencyresponse_pr/emergencyresponse_pr/settings.py
+++ emergencyresponse_pr/emergencyresponse_pr/settings.py
@@ -16,7 +16,7 @@ from decouple import Config, RepositoryEnv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-config = Config(RepositoryEnv(BASE_DIR / '.secrete-keys'))
+config = Config(RepositoryEnv(BASE_DIR / '.env'))
 
 
 # Quick-start development settings - unsuitable for production
