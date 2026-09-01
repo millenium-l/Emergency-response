a0e5bdd19f7bd4fbdf13ba1c53e7d80a6b9c29d1
millenium-1 <louiskaroki2010@gmail.com>
2026-09-01 10:50:09 +0300

    modified settings.py to add truehost as allowed hosts

--- emergencyresponse_pr/emergencyresponse_pr/settings.py
+++ emergencyresponse_pr/emergencyresponse_pr/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['KainguMike.pythonanywhere.com', 'rick2000.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['KainguMike.pythonanywhere.com', 'rick2000.pythonanywhere.com', 'localhost', '127.0.0.1', 'emergencynow.co.ke', 'www.emergencynow.co.ke']
 
 # Security settings
 SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin"
